a06ab6431e27630b9240f0c73db0d12b965cff92
Ajay Pandey <pandey7ajay@gmail.com>
2023-12-16 15:49:00 +0530

    Reverted few changes

--- src/components/GoogleAuthComponent.tsx
+++ src/components/GoogleAuthComponent.tsx
@@ -1,7 +1,6 @@
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 export const GoogleAuthComponent = () => {
-  
   const handleSuccess = (credentialResponse: any) => {
     console.log(credentialResponse);
   };
@@ -11,12 +10,13 @@ export const GoogleAuthComponent = () => {
   };
 
   return (
-    <div >
+    <div style = {{display: "none"}}>
       <GoogleOAuthProvider clientId="406061486952-6r67hp2bqmmqj9s8lbfok3lv4t04oqbg.apps.googleusercontent.com">
         <GoogleLogin
           onSuccess={handleSuccess}
           onError={handleError}
           useOneTap
+          auto_select
           theme="filled_blue"
           logo_alignment="left"
         />
