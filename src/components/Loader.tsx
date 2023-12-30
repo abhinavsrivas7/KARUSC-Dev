5c3c10f2e0104effbacc6127cc2fce644c5b6e95
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2023-12-30 21:06:18 +0530

    new full dtsck structure

--- src/components/Loader.tsx
+++
@@ -1,6 +0,0 @@
-import { Container } from "react-bootstrap";
-import { TailSpin } from "react-loader-spinner";
-
-export const Loader = () => <Container className="d-flex justify-content-center align-items-center">
-    <TailSpin color="white" height={70} width={70} />
-</Container>;
--- src/utilities/ProductCardUtils.ts
+++
@@ -1,5 +0,0 @@
-const FORMATTER = new Intl.NumberFormat(undefined, { currency: "INR", style: "currency" });
-
-export const FormatProductTitle = (title: string) => title.length > 30 ? title.substring(0, 28).concat("...") : title;
-
-export const FormatProductPrice = (price: number) => FORMATTER.format(price);
