43841d0451f98b0e1d819c7a663064d7a4855a7b
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2023-12-31 12:09:07 +0530

    refactoring

--- Karusc.Server/Program.cs
+++ Karusc.Server/Program.cs
@@ -1,16 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var corsPolicy = "dev-cors";
-// Add services to the container.
 builder.Services.AddCors(options => 
     options.AddPolicy(corsPolicy, policy => policy
         .WithOrigins("https://localhost:5173")
-        //.AllowAnyOrigin()
         .AllowAnyHeader()
         .AllowAnyMethod()));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,21 +15,12 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 app.UseCors(corsPolicy);
-
 app.MapFallbackToFile("/index.html");
 
 app.Run();
