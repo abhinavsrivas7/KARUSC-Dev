3db6e288fb002bfe42af626439cf26c5ced7bbb4
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-01 03:43:36 +0530

    added mediatr

---
+++ Karusc.Server.Application/ApplicationStartup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Karusc.Server.Application
+{
+    public static class ApplicationStartup
+    {
+        public static void AddApplication(this IServiceCollection services) =>
+            services.AddMediatR(configuration => configuration
+                .RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    }
+}
