74241c956096e8bfab3a634e5251976f9bfc5cc0
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-01 04:14:08 +0530

    migrated get all products api to clean architecture

---
+++ Karusc.Server.Application/Products/GetAllProducts/GetAllProductsRequest.cs
@@ -0,0 +1,9 @@
+﻿using Karusc.Server.Domain;
+using MediatR;
+
+namespace Karusc.Server.Application.Products.GetAllProducts
+{
+    public record GetAllProductsRequest(
+        int pageSize,
+        int pageNumber) : IRequest<List<Product>>;
+}
--- Karusc.Server/Controllers/ProductController.cs
+++ Karusc.Server/Controllers/ProductController.cs
@@ -1,23 +1,18 @@
+using Karusc.Server.Application.Products.GetAllProducts;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Karusc.Server.Controllers
 {
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
-    { 
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator) => _mediator = mediator;
+
         [HttpGet]
-        public IActionResult Get() => Ok(
-        new List<Product> 
-        { 
-            new()
-            {
-               ID = 1,
-               Title = "Tshirt",
-               Description = "Cool tshirt",
-               Category = "Clothes",
-               Price = 30.24,
-               Image = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-            }
-        });
+        public async Task<IActionResult> Get(int pageSize = 0, int pageNumber = 0) => 
+            Ok(await _mediator.Send(new GetAllProductsRequest(pageSize, pageNumber)));
     }
 }
