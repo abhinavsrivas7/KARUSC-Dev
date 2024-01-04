44a1e109cbc80abccf7f3405a16dd1335a997346
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-05 00:07:33 +0530

    added changes for reading connection string from environment variables and pipeline updates

--- Karusc.Server.Infrastructure/InfrastructureStartup.cs
+++ Karusc.Server.Infrastructure/InfrastructureStartup.cs
@@ -3,19 +3,19 @@ using Karusc.Server.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using System.Reflection;
 
 namespace Karusc.Server.Infrastructure
 {
     public static class InfrastructureStartup
     {
-        private const string _connectionStringName = "KaruscDB";
+        private const string _connectionStringConfigName = "KaruscDB";
+        private const string _connectionStringEnvironmentVariable = "KARUSC_DB_CONNECTION_STRING";
 
-        private static void AddPersistence(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        private static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString(_connectionStringName);
+            var connectionString = GetConnectionString(configuration);
             
             services.AddDbContextPool<KaruscDbContext>(options => options.UseMySql(
                 connectionString,
@@ -25,9 +25,20 @@ namespace Karusc.Server.Infrastructure
             services.AddScoped<IKaruscDbContext, KaruscDbContext>();
         }
 
-        public static void AddInfrastructure(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        private static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(_connectionStringConfigName);
+
+            connectionString = string.IsNullOrEmpty(connectionString)
+                ? configuration[_connectionStringEnvironmentVariable]
+                : connectionString;
+
+            return !string.IsNullOrEmpty(connectionString) 
+                ? connectionString 
+                : throw new KeyNotFoundException("connection string");
+        }
+
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             AddPersistence(services, configuration);
         }
