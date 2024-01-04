902f119810ab3042192800afd8a839970f0cb1fa
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-04 10:42:21 +0530

    changes for ef core and readme

--- Karusc.Server.Application/Products/GetAllProducts/GetAllProductsRequestHandler.cs
+++ Karusc.Server.Application/Products/GetAllProducts/GetAllProductsRequestHandler.cs
@@ -1,27 +1,23 @@
-﻿using Karusc.Server.Domain;
+﻿using Karusc.Server.Application.Contracts;
+using Karusc.Server.Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Karusc.Server.Application.Products.GetAllProducts
 {
-    public record GetAllProductsRequestHandler : 
+    public class GetAllProductsRequestHandler : 
         IRequestHandler<GetAllProductsRequest, List<Product>>
     {
+        private readonly IKaruscDbContext _context;
+
+        public GetAllProductsRequestHandler(IKaruscDbContext context) => 
+            _context = context;
+
         public async Task<List<Product>> Handle(
             GetAllProductsRequest request, 
-            CancellationToken cancellationToken)
-        {
-            await Task.Delay(1);
-            
-            return new List<Product>() 
-            {
-                new(
-                    1,
-                    "Tshirt",
-                    30.20,
-                    "cool tshirt",
-                    "clothes",
-                    "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg")
-            };
-        }
+            CancellationToken cancellationToken) => await _context.Products
+                .Skip(request.pageSize * request.pageNumber)
+                .Take(request.pageSize)
+                .ToListAsync();
     }
 }
