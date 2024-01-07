4e7cd9164b5a7a2049b69bb6db7880790d587a7e
Ajay Pandey <pandey7ajay@gmail.com>
2024-01-07 11:09:47 +0530

    Create and Read Products Swagger Impl

---
+++ Karusc.Server.Application/Products/GetById/GetProductByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Karusc.Server.Domain;
+using MediatR;
+
+namespace Karusc.Server.Application.Products.GetById
+{
+    public record GetProductByIdQuery(Guid Id) : IRequest<Product>;
+}
--- Karusc.Server.Domain/Product.cs
+++ Karusc.Server.Domain/Product.cs
@@ -1,10 +1,39 @@
 ﻿namespace Karusc.Server.Domain
 {
-    public record Product(
-        int ID,
-        string? Title,
-        double Price,
-        string? Description,
-        string? Category,
-        string? Image);
+    public class Product
+    {
+        public Guid Id { get; init; }
+        public string Title { get; init; }
+        public decimal Price { get; init; }
+        public string Description { get; init; }
+        public string Category { get; init; }
+        public string Image { get; init; }
+
+        private Product(
+            Guid id,
+            string title,
+            decimal price,
+            string description,
+            string category,
+            string image)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+            Description = description;
+            Category = category;
+            Image = image;
+        }
+
+        public static Product Create(
+            string title,
+            decimal price,
+            string description,
+            string category,
+            string image)
+        {
+            if(price < 0) throw new InvalidOperationException("price cant be less than 0");
+            return new Product(Guid.NewGuid(), title, price, description, category, image);
+        }
+    }
 }
