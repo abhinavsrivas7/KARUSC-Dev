8f10027761129a608ea91a1e893c69d95c065e8b
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-14 14:30:59 +0530

    added scroll bar overlay element

--- karusc.client/src/App.tsx
+++ karusc.client/src/App.tsx
@@ -6,18 +6,29 @@ import { Navbar } from "./components/Common/Navbar";
 import { Footer } from "./components/Common/Footer";
 import { Banner } from "./components/Home/Banner";
 import { ScreenSizeProvider } from "./context/ScreenSizeContext";
+import { useOverlayScrollbars } from "overlayscrollbars-react";
+import { useEffect } from "react";
 
-const App = () => <ScreenSizeProvider>
-    <Banner title="Free shipping within India, on orders above INR 1000."></Banner>
-    <Navbar />
-    <div className="light-pink" style={{ paddingRight: '0px', paddingLeft: '0px', }}>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
-    </div>
-    <Footer></Footer>
-</ScreenSizeProvider>;
+const App = () => {
+    const [initBodyOverlayScrollbars] = useOverlayScrollbars({
+        defer: true,     
+        options: { scrollbars: { theme: "os-theme-dark", autoHide: "scroll", autoHideDelay: 500 }},
+    });
+
+    useEffect(() => initBodyOverlayScrollbars(document.body), [initBodyOverlayScrollbars]);
+    
+    return <ScreenSizeProvider>
+        <Banner title="Free shipping within India, on orders above INR 1000."></Banner>
+        <Navbar />
+        <div className="light-pink" style={{ paddingRight: '0px', paddingLeft: '0px', }}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+        <Footer></Footer>
+    </ScreenSizeProvider>
+};
 
 export default App;
