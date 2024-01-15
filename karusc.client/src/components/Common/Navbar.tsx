7af3c1c529f2ac2b6345f0a1cf4281cec9f17354
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-01-15 22:02:12 +0530

    removed scroll bar and made navbar consistent on all screens

--- karusc.client/src/components/Common/Navbar.tsx
+++ karusc.client/src/components/Common/Navbar.tsx
@@ -1,80 +1,62 @@
-import { useState } from "react";
-import { Button, Col, Container, Form, Nav, Navbar as NavbarBs, Row } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { SearchBar } from "./SearchBar";
+import { Button, Container, Nav, Navbar as NavbarBs, } from "react-bootstrap";
+import backArrowImg from "../../../resources/media/backArrow.svg";
 import userImg from "../../../resources/media/user.svg";
 import hamburgImg from "../../../resources/media/hamburg.svg";
 import searchImg from "../../../resources/media/search.svg";
 import cartImg from "../../../resources/media/cart.svg";
-import backArrowImg from "../../../resources/media/backArrow.svg";
-import { useScreenSize } from "../../context/ScreenSizeContext";
-import { DeviceTypes } from "../../models/DeviceTypes";
+import { useState } from "react";
+import { SearchBar } from "./SearchBar";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     const [isSearchActive, setSearchActive] = useState<boolean>(false);
-    const { getDeviceType } = useScreenSize();
 
     const searchInactiveLayout = <>
-        <Button 
-            style={{ width: "3rem", height: "3rem", position: "relative" }} 
+        <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="white"
             className="mb-1">
-            <img src={hamburgImg}/>
+            <img src={hamburgImg} />
         </Button>
         <Nav className="me-auto">
             <Nav.Link to="/" as={NavLink} style={{ fontSize: "1.25em" }} className="light-pink semi-bold-font">
                 <strong>KARUSC</strong>
             </Nav.Link>
         </Nav>
-        {getDeviceType() == DeviceTypes.MOBILE
-            ? <Button 
-                style={{ width: "2.25rem", height: "3rem", position: "relative" }} 
+        <Button
+                style={{ width: "2.25rem", height: "3rem", position: "relative" }}
                 variant="white"
                 onClick={() => setSearchActive(true)}>
                 <img src={searchImg} />
-              </Button>
-            : <Form>
-                <Row>
-                    <Col xs="auto"  style={{position: "relative", transform: "translate(27%, 0%)"}}>
-                        <Form.Control
-                            type="text"
-                            placeholder="Search"
-                            className=" mr-sm-2 search-input custom-placeholder" />
-                    </Col>
-                    <Col xs="auto" style={{position: "relative"}}>
-                        <Button variant="white" style={{width: "2.25rem",height: "2.25rem"}}>
-                            <img src = {searchImg} />
-                        </Button>
-                    </Col>
-                </Row>
-               </Form>}       
-        <Button 
-            style={{ width: "3rem", height: "3rem", position: "relative" }} 
+        </Button>
+        <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="white"
             className="mb-1 ms-3">
-            <img src={userImg} height="22rem"/>
+            <img src={userImg} height="22rem" />
         </Button>
-        <Button 
-            style={{ width: "3rem", height: "3rem", position: "relative" }} 
+        <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="white">
-            <img src = {cartImg} />
-        </Button> 
+            <img src={cartImg} />
+        </Button>
+    </>;
+
+    const searchActiveLayout = <>
+        <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            variant="white"
+            className="mb-1"
+            onClick={() => setSearchActive(false)}>
+            <img src={backArrowImg} />
+        </Button>
+        <SearchBar />
     </>;
 
     return <NavbarBs className="shadow-sm mt-4 pb-0 light-pink" sticky="top">
         <Container>
-            {isSearchActive 
-                ? <>
-                    <Button 
-                        style={{ width: "3rem", height: "3rem", position: "relative"}} 
-                        variant="white"
-                        className="mb-1"
-                        onClick={() => setSearchActive(false)}>
-                        <img src = {backArrowImg} />
-                    </Button>
-                    <SearchBar />
-                </>
-                : searchInactiveLayout}               
+            {isSearchActive ? searchActiveLayout : searchInactiveLayout}
         </Container>
     </NavbarBs>;
-};
+}
+
