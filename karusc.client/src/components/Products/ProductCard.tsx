23e8fac5b5304cbe4a0495d5b8a6bde109ad3445
Ajay Pandey <pandey7ajay@gmail.com>
2024-02-05 20:27:19 +0530

    Product Page Changes

--- karusc.client/src/components/Products/ProductCard.tsx
+++ karusc.client/src/components/Products/ProductCard.tsx
@@ -2,18 +2,18 @@ import {Card } from "react-bootstrap";
 import { Product } from "../../models/Product";
 import { FormatProductPrice, FormatProductTitle } from "../../utilities/ProductCardUtils";
 
-export const ProductCard = (product: Product) => <Card className="h-100" style={{ cursor: "pointer" }}>
-    <Card.Img variant="top" src={product.images[0]} height="150px" style={{objectFit: "cover"}}/>
-    <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4 bold-font">
-            <div>
-                <div className="light-pink" title={product.title}>
-                    {FormatProductTitle(product.title)}
-                </div>
-                <div className="mt-2 light-pink">
+export const ProductCard = (product: Product) => { 
+    return <>
+    <Card style={{ border: "none" }}>
+        <Card.Img variant="top" src={product.images[0]} style={{ borderRadius: 0 }} />
+            <Card.Body className="light-pink">
+                <Card.Text className="d-flex justify-content-begin align-items-center regular-font">
+                        {FormatProductTitle(product.title)}
+                </Card.Text>
+                <Card.Text className="d-flex justify-content-begin align-items-center regular-font">
                     {FormatProductPrice(product.price)}
-                </div>
-            </div>
-        </Card.Title>
-    </Card.Body>
-</Card>;
+                </Card.Text>
+            </Card.Body>
+    </Card>
+</>
+}
--- karusc.client/src/pages/Shop.tsx
+++ karusc.client/src/pages/Shop.tsx
@@ -7,11 +7,20 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import { ProductCard } from "../components/Products/ProductCard";
 import filterImg from "../../resources/media/filter.svg";
 import { GetProductsEndpoint } from "../utilities/EndpointUtils";
+import { useScreenSize } from "../context/ScreenSizeContext";
+import { DeviceTypes } from "../models/DeviceTypes";
+import { NavLink } from "react-router-dom";
+
 
 
 export const Shop = () => {
     const [products, setProducts] = useState<Product[]>([]);
     const [errorState, setErrorState] = useState<boolean>(false);
+    const { getDeviceType } = useScreenSize();
+    const gapVal = getDeviceType() == DeviceTypes.DESKTOP ? 0 : 1;
+    const gapClass = getDeviceType() == DeviceTypes.DESKTOP ? "my -2 px-3" : "my-2 px-1";
+    const link = "/shop?product=";
+
     //console.log(window.location.href
     //    .substring(window.location.href.indexOf('?') + 1)
     //    .split('&')
@@ -32,18 +41,33 @@ export const Shop = () => {
     }, []);
     
     return products 
-        ? <>
+        ?
+        <>
             <Container className="d-flex justify-content-center align-items-center">
                 <h1 className="bold-font light-pink">KARUSC</h1>
             </Container>
             <hr style={{color: "white"}}/>
-            <Row md={3} xs={2} lg={4} className="g-3">
-                {products.map(product => <Col key={product.id}><ProductCard {...product}/></Col>)}
-            </Row>
-            <Button 
-                style={{position: "fixed", right: "2rem", bottom: "2rem", height: "3rem", width: "3rem"}}
+            <Container className="mt-4">
+                <Row md={4} xs={3} lg={4} gap={gapVal}>
+                    {products.map((item) => (
+                        <Col key={item.id} className={gapClass}>
+                            <NavLink style={{ textDecoration: 'none', width: '100%' }} to={link + item.id}>
+                                <ProductCard
+                                    images={item.images}
+                                    title={item.title}
+                                    price={item.price}
+                                    id={item.id}
+                                    description={item.description}
+                                    category={item.category}/>
+                            </NavLink>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+            <Button
+                style={{ position: "fixed", right: "2rem", bottom: "2rem", height: "3rem", width: "3rem" }}
                 variant="dark">
-                <img src = {filterImg} />
+                <img src={filterImg} />
             </Button>
           </>
         : errorState
