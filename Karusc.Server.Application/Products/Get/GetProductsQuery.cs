995e662ed2822c3fa41e377946d647c3590e4970
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-02-10 20:47:20 +0530

    returned count in get products api

--- Karusc.Server.Application/Products/Get/GetProductsQuery.cs
+++ Karusc.Server.Application/Products/Get/GetProductsQuery.cs
@@ -6,23 +6,24 @@ namespace Karusc.Server.Application.Products.Get
         int PageSize, 
         int PageNumber,
         HashSet<Guid> Categories, 
-        HashSet<Guid> Collections) : IRequest<List<ProductDto>>
+        HashSet<Guid> Collections) : IRequest<ProductWithCountDto>
     {
-        public GetProductsQuery(int pageSize,
-        int pageNumber,
-        string categories,
-        string collections) : this(
-            pageSize, 
-            pageNumber,
-            categories
-                .Split(',')
-                .Where(category => !string.IsNullOrEmpty(category))
-                .Select(category => new Guid(category))
-                .ToHashSet(),
-            collections
-                .Split(',')
-                .Where(collection => !string.IsNullOrEmpty(collection))
-                .Select(collection => new Guid(collection))
-                .ToHashSet()) { }
+        public GetProductsQuery(
+            int pageSize,
+            int pageNumber,
+            string categories,
+            string collections) : this(
+                pageSize, 
+                pageNumber,
+                categories
+                    .Split(',')
+                    .Where(category => !string.IsNullOrEmpty(category))
+                    .Select(category => new Guid(category))
+                    .ToHashSet(),
+                collections
+                    .Split(',')
+                    .Where(collection => !string.IsNullOrEmpty(collection))
+                    .Select(collection => new Guid(collection))
+                    .ToHashSet()) { }
     };
 }
---
+++ Karusc.Server.Application/Products/ProductWithCountDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Karusc.Server.Application.Products
+{
+    internal record ProductWithCountDto(
+        List<ProductDto> Products, 
+        int Count);
+}
