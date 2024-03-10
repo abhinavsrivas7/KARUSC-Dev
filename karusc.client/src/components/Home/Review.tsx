2ef9ae5434df6fd891b85f06cf84bf4e8d42452b
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-03-10 21:01:45 +0530

    width of review cards increased

--- karusc.client/src/components/Home/Review.tsx
+++ karusc.client/src/components/Home/Review.tsx
@@ -11,8 +11,8 @@ import { useUserContext } from '../../hooks/useUserHook';
 
 const responsive = {
     desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3.5, slidesToSlide: 4 },
-    tablet: { breakpoint: { max: 1024, min: 464 }, items: 3.5, slidesToSlide: 2 },
-    mobile: { breakpoint: { max: 464, min: 350 }, items: 2.5, slidesToSlide: 4 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 2.5, slidesToSlide: 2 },
+    mobile: { breakpoint: { max: 464, min: 350 }, items: 1.5, slidesToSlide: 4 },
     narrowMobile: { breakpoint: { max: 350, min: 0 }, items: 1.5, slidesToSlide: 4 }
 };
 
